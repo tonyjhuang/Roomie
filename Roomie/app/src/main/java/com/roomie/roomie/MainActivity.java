ed9c6b42610110ca2ddef7cc275a8636e5aadc7c
Tony Huang <tony.huang.jun@gmail.com>
2015-10-10 01:40:24 -0700

    autocomplete

--- Roomie/app/src/main/java/com/roomie/roomie/MainActivity.java
+++ Roomie/app/src/main/java/com/roomie/roomie/MainActivity.java
@@ -1,31 +1,122 @@
 package com.roomie.roomie;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final LatLngBounds BOUNDS_USA =
+            new LatLngBounds(new LatLng(25.284438, -125.859375), new LatLng(48.545705, -66.093750));
+
+    /**
+     * GoogleApiClient wraps our service connection to Google Play Services and provides access
+     * to the user's sign in state as well as the Google's APIs.
+     */
+    protected GoogleApiClient googleApiClient;
+
+    private PlaceAutocompleteAdapter autocompleteAdapter;
+
+    private AutoCompleteTextView autoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Construct a GoogleApiClient for the {@link Places#GEO_DATA_API} using AutoManage
+        // functionality, which automatically sets up the API client to handle Activity lifecycle
+        // events. If your activity does not extend FragmentActivity, make sure to call connect()
+        // and disconnect() explicitly.
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        Log.d("Main", "connected to api");
+                        Log.d("Main", getLatLng("brooklyn, nyc").toString());
+                        Log.d("Main", getLatLng("brooklyn").toString());
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
+
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        // Retrieve the AutoCompleteTextView that will display Place suggestions.
+        autoCompleteTextView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete_places);
+
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        autocompleteAdapter = new PlaceAutocompleteAdapter(this, googleApiClient, BOUNDS_USA,
+                null);
+
+        // Register a listener that receives callbacks when a suggestion has been selected
+        autoCompleteTextView.setOnItemClickListener(Autocomplete.getClickListener(
+                googleApiClient, autocompleteAdapter, new Autocomplete.Callback() {
+                    @Override
+                    public void onResult(Place place) {
+                        Log.d(TAG, getLatLng(place.getAddress().toString()).toString());
+                    }
+                }));
+
+        autoCompleteTextView.setAdapter(autocompleteAdapter);
+
+    }
+
+    private LatLng getLatLng(String location) {
+        Geocoder coder = new Geocoder(this);
+        try {
+            List<Address> addressList = coder.getFromLocationName(location, 1);
+            if (addressList.size() != 0) {
+                return new LatLng(
+                        addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
+            } else {
+                return null;
             }
-        });
+        } catch (IOException e) {
+            Log.e("Main", e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
+
+        // TODO(Developer): Check error code and notify the user of error state and resolution.
+        Toast.makeText(this,
+                "Could not connect to Google API Client: Error " + connectionResult.getErrorCode(),
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -43,7 +134,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_messages) {
+            //TODO(tony): go to messages screen.
             return true;
         }
 
